a2f13dd4c5a10df073fdd22fe39aad628eec93b2
ariadiprana <ari.adiprana@gmail.com>
2016-10-05 14:28:10 +0700

    release

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,15 +1,57 @@
 var Items = require('../models/items')
 
 module.exports = {
-  insert: function(req,res,next){
-      var items = new Items({
-        code:req.body.code,
-        name:req.body.name,
-        description:req.body.description,
-        price:req.body.price,
-        stock:req.body.stock
+  insert: insert,
+  display: display,
+  update:update,
+  hapus:hapus
+}
+
+function hapus(req,res,next){
+  Items.findOne({
+    _id:req.params.id
+  },(err,items) => {
+      if(err)throw err
+
+      items.remove((err)=> {
+        if(err) throw err
+        res.json(items)
       })
-      items.save()
-      res.json(items)
-  }
+  })
+}
+
+function update(req,res,next){
+  Items.findOne({
+    _id:req.params.id
+  },(err,items) => {
+      items.code = req.body.code
+      items.name = req.body.name
+      items.description = req.body.description
+      items.price = req.body.price
+      items.stock = req.body.stock
+
+      items.save((err)=> {
+        if(err) throw err
+        res.json(items)
+      })
+  })
+}
+
+function display(req,res,next){
+    Items.find({},(err,result) => {
+          res.json(result)
+    })
+}
+
+
+function insert(req,res,next){
+    var items = new Items({
+      code:req.body.code,
+      name:req.body.name,
+      description:req.body.description,
+      price:req.body.price,
+      stock:req.body.stock
+    })
+    items.save()
+    res.json(items)
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,5 +4,9 @@ const itemsController = require('../controllers/items')
 
 
 router.post('/items', itemsController.insert)
+router.get('/items', itemsController.display)
+router.put('/items/:id', itemsController.update)
+router.delete('/items/:id', itemsController.hapus)
+
 
 module.exports = router
